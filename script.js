d4f318290ff6be4cd0ddfd254a16243303530302
luirodrigu <luisermrodriguez@gmail.com>
2025-05-17 03:13:43 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// Función para abrir la ventana emergente
+function abrirVentana() {
+  document.getElementById("ventanaEmergente").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentana() {
+  document.getElementById("ventanaEmergente").style.display = "none";
+}
+
+// Función para abrir la ventana emergente
+function abrirVentanaEmergencia() {
+  document.getElementById("ventanaEmergenteEmergencia").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentanaEmergencia() {
+  document.getElementById("ventanaEmergenteEmergencia").style.display = "none";
+}
+
+
+// Función para abrir la ventana emergente
+function abrirVentana_dos() {
+  document.getElementById("ventanaEmergente_dos").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentana_dos() {
+  document.getElementById("ventanaEmergente_dos").style.display = "none";
+}
+
+// Función para abrir la ventana emergente
+function abrirVentana_tres() {
+  document.getElementById("ventanaEmergente_tres").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentana_tres() {
+  document.getElementById("ventanaEmergente_tres").style.display = "none";
+}
+
+
+
+function llamarEmergencia() {
+  alert("Llamando a emergencias...");
+}
+
+function llamarFamiliar() {
+  alert("Llamando a un familiar...");
+}
+
+
+
+
