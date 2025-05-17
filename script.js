87542b6e0114de079b62a1a3dc21cf1aaa7d700a
luirodrigu <luisermrodriguez@gmail.com>
2025-05-17 04:34:02 -0600

    avanceees

--- script.js
+++ script.js
@@ -39,6 +39,24 @@ function cerrarVentana_tres() {
   document.getElementById("ventanaEmergente_tres").style.display = "none";
 }
 
+function abrirVentana_cuatro() {
+  document.getElementById("ventanaEmergente_cuatro").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentana_cuatro() {
+  document.getElementById("ventanaEmergente_cuatro").style.display = "none";
+}
+
+function abrirVentana_cinco() {
+  document.getElementById("ventanaEmergente_cinco").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentana_cinco() {
+  document.getElementById("ventanaEmergente_cinco").style.display = "none";
+}
+
 
 
 function llamarEmergencia() {
@@ -47,6 +65,25 @@ function llamarEmergencia() {
 
 function llamarFamiliar() {
   alert("Llamando a un familiar...");
+}
+
+
+// Función para modificar contactos de emergencia
+function modificarContactos() {
+  alert("Accediendo a la sección para modificar contactos de emergencia.");
+  // Aquí puedes añadir la lógica para redirigir al usuario o abrir un formulario
+}
+
+// Función para acceder a los ajustes de la app
+function entrarAjustes() {
+  alert("Accediendo a los ajustes de la app.");
+  // Aquí puedes redirigir a los ajustes de la app o abrir un panel de configuraciones
+}
+
+// Función para gestionar métodos de pago
+function gestionarMetodosPago() {
+  alert("Accediendo a la sección de métodos de pago.");
+  // Aquí puedes redirigir a una página de métodos de pago o abrir un formulario
 }
 
 
